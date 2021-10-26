5bd1d6ef8f1890a65b12b6212e6761a9c223ea42
Michael Fellinger <michael.fellinger@iohk.io>
2021-10-26 16:45:21 +0200

    move closure.nix into go

--- nix/nspawn.go
+++ nix/nspawn.go
@@ -33,6 +33,44 @@ const (
 
 	TarImage string = "tar"
 	RawImage string = "raw"
+
+	closureNix = `
+{ flakes }:
+let
+  nixpkgs = builtins.getFlake "github:nixos/nixpkgs/nixos-21.05";
+  inherit (nixpkgs.legacyPackages.x86_64-linux) lib buildPackages;
+  inherit (builtins) match elemAt getFlake length fromJSON;
+
+  resolve = flakeURI:
+    let
+      split = match "^([^#]+)#(.*)$" flakeURI;
+      flake = elemAt split 0;
+      attr = elemAt split 1;
+      path = lib.splitString "." attr;
+      root = getFlake flake;
+
+      paths = [
+        path
+        ([ "packages" "x86_64-linux" ] ++ path)
+        ([ "legacyPackages" "x86_64-linux" ] ++ path)
+      ];
+
+      findRoute = route:
+        if (lib.hasAttrByPath route root) then
+          lib.getAttrFromPath route root
+        else
+          null;
+
+      notNull = e: e != null;
+      allFound = lib.filter notNull (map findRoute paths);
+    in if (length allFound) > 0 then
+      elemAt allFound 0
+    else
+      throw "No attribute '${attr}' in flake '${flake}' found";
+
+  drvs = map resolve (fromJSON flakes);
+in buildPackages.closureInfo { rootPaths = drvs; }
+`
 )
 
 var (
@@ -580,7 +618,8 @@ func nixBuildClosure(flakes []string, link string) (string, error) {
 	cmd := exec.Command(
 		"nix", "build",
 		"--out-link", link,
-		"-f", "./closure.nix",
+		"--expr", closureNix,
+		"--impure",
 		"--argstr", "flakes", string(j))
 
 	stderr := &bytes.Buffer{}
